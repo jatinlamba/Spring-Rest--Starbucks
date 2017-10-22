a158168d6aaa91b5161318e6f34756f4e83ec7e8
Jatin Lamba <jatinlamba@users.noreply.github.com>
2017-10-22 16:39:49 -0500

    Create StoreRepositoryIntegrationTests.java

---
+++ src/test/java/example/springdata/rest/stores/StoreRepositoryIntegrationTests.java
@@ -0,0 +1,53 @@
+
+package example.springdata.rest.stores;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import example.springdata.rest.stores.Store.Address;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Metrics;
+import org.springframework.data.geo.Point;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Integration tests for {@link StoreRepository}.
+ * 
+ * @author Jatin Lamba
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class StoreRepositoryIntegrationTests {
+
+	@Autowired StoreRepository repository;
+
+	@Before
+	@After
+	public void clearDb() {
+		repository.deleteAll();
+	}
+
+	@Test
+	public void findsStoresByLocation() {
+
+		Point location = new Point(-73.995146, 40.740337);
+		Store store = new Store("Foo", new Address("street", "city", "zip", location));
+
+		store = repository.save(store);
+
+		Page<Store> stores = repository.findByAddressLocationNear(location, new Distance(1.0, Metrics.KILOMETERS),
+				new PageRequest(0, 10));
+
+		assertThat(stores.getContent(), hasSize(1));
+		assertThat(stores.getContent(), hasItem(store));
+	}
+}
