0ab56cca973450a10fbc652602a1433bb0715bc6
Jatin Lamba <jatinlamba@users.noreply.github.com>
2017-10-22 16:21:40 -0500

    Create Application.java

---
+++ src/main/java/example/springdata/rest/stores/Application.java
@@ -0,0 +1,17 @@
+package example.springdata.rest.stores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring configuration class main application bootstrap point.
+ * 
+ * @author Jatin Lamba
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
